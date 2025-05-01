43797a9f49fda38778490bc51b5d0fb48f9b8ce9
adarks <danieladarkwah339@gmail.com>
2025-05-01 11:20:33 +0000

    Add an array of components

---
+++ main.js
@@ -0,0 +1,26 @@
+//Use array to store different components
+const signs = [
+  "Aries",
+  "Taurus",
+  "Gemini",
+  "Cancer",
+  "Leo",
+  "Virgo",
+  "Libra",
+  "Scorpio",
+  "Sagittarius",
+  "Capricorn",
+  "Aquarius",
+  "Pisces",
+];
+const moods = [
+  "happy",
+  "adventurous",
+  "calm",
+  "confident",
+  "inspired",
+  "focused",
+];
+const luckyNumbers = [3, 7, 9, 14, 21, 25];
+
+//Create function to randomly select components
